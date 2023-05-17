d2b2d6d7565476885e74d6ea24b5db1300f2a50e
Ido Bar <ido1403@gmail.com>
2023-05-17 13:56:55 +0300

    fix deploy

--- http_server.py
+++ http_server.py
@@ -44,6 +44,14 @@ def get_guides_by_domain(domain):
         return jsonify(guides_list)
     else:
         return jsonify({"message": "No guides found for the specified domain"}), 404
+@app.route('/guides', methods=['GET'])
+def get_guides():
+    guides = guides_collection.find({"_id": False})
+    guides_list = list(guides)
+    if guides_list:
+        return jsonify(guides_list)
+    else:
+        return jsonify({"message": "No guides found for the specified domain"}), 404
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port = 5000, debug=True)
